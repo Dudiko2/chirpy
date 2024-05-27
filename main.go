9e77308d35a972bfd095bf60ab41013ffe03326d
david <dudiko2@gmail.com>
2024-05-27 19:29:59 +0300

    completed step

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	rootDirPath := http.Dir(".")
+	appPath := "/app/"
+	port := "8080"
 	mux := http.NewServeMux()
+	mux.Handle(appPath+"*",
+		http.StripPrefix(appPath, http.FileServer(rootDirPath)))
+	mux.HandleFunc("/healthz", handlerHealth)
 	server := http.Server{
-		Addr:    "localhost:8080",
+		Addr:    "localhost:" + port,
 		Handler: mux,
 	}
+	log.Printf("Listening on port: %s\n", port)
 	server.ListenAndServe()
+}
+
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
