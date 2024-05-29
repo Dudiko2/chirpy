d164c19bc11ab2622cd3cd6dc4add4b1ace15b9f
david <dudiko2@gmail.com>
2024-05-29 22:22:18 +0300

    steps completed

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,16 +19,78 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(200)
-	hits := fmt.Sprintf("Hits: %v", cfg.fileserverHits)
-	w.Write([]byte(hits))
+	tmpl := `<html>
+
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	
+	</html>
+	`
+	content := fmt.Sprintf(tmpl, cfg.fileserverHits)
+	w.Write([]byte(content))
 }
 
 func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, _ *http.Request) {
 	cfg.fileserverHits = 0
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorRes struct {
+		Error string `json:"error"`
+	}
+	res := errorRes{
+		Error: msg,
+	}
+	respondWithJSON(w, code, res)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type reqBody struct {
+		Body string `json:"body"`
+	}
+	type resBody struct {
+		Valid bool `json:"valid"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := reqBody{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Internal error")
+		return
+	}
+	textLen := len(params.Body)
+	if textLen > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+	res := resBody{
+		Valid: true,
+	}
+	respondWithJSON(w, http.StatusOK, res)
 }
 
 func main() {
@@ -41,19 +104,14 @@ func main() {
 	fsHandler := http.StripPrefix(appPath,
 		apiCfg.middlewareMetricsInc(http.FileServer(rootDirPath)))
 	mux.Handle(appPath+"*", fsHandler)
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("GET /api/reset", apiCfg.handlerResetMetrics)
 	mux.HandleFunc("GET /api/healthz", handlerHealth)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	server := http.Server{
 		Addr:    "localhost:" + port,
 		Handler: mux,
 	}
 	log.Printf("Listening on port: %s\n", port)
 	server.ListenAndServe()
-}
-
-func handlerHealth(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte("OK"))
 }
